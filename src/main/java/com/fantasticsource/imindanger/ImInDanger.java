db8c05b3e76e755daed0ce3c30251ae9d29a0183
Laike-Endaril <silvertallon@gmail.com>
2025-03-16 14:28:05 -0700

    Initial commit

---
+++ src/main/java/com/fantasticsource/imindanger/ImInDanger.java
@@ -0,0 +1,29 @@
+package com.fantasticsource.imindanger;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = ImInDanger.MODID, name = ImInDanger.NAME, version = ImInDanger.VERSION)
+public class ImInDanger
+{
+    public static final String MODID = "imindanger";
+    public static final String NAME = "I'm In Danger!";
+    public static final String VERSION = "1.12.2.000";
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(ImInDanger.class);
+    }
+
+    @SubscribeEvent
+    public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+}
