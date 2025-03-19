b1bc84d3a12921628b21e312b351cfff8737d6ac
Laike-Endaril <silvertallon@gmail.com>
2025-03-19 16:31:16 -0700

    1.12.2.003

--- src/main/java/com/fantasticsource/imindanger/ImInDanger.java
+++ src/main/java/com/fantasticsource/imindanger/ImInDanger.java
@@ -46,7 +46,7 @@ public class ImInDanger
 {
     public static final String MODID = "imindanger";
     public static final String NAME = "I'm In Danger!";
-    public static final String VERSION = "1.12.2.002";
+    public static final String VERSION = "1.12.2.003";
 
     private static final ResourceLocation DANGER_INDICATOR_TEXTURE = new ResourceLocation(MODID, "image/danger.png");
 
